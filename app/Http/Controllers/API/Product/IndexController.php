e2caef90fd900bc68ff644a6e283df5b9f8eb5fb
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-10-09 08:30:14 +0300

    add new category via color and optimizide view product

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Product\IndexProductResource;
-use App\Http\Resources\Product\ProductResource;
-use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
